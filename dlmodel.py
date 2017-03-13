5939d558788728ee53f1ae0dc4a55f7b3331dfa1
pthoreho <pruthvi.gowda.thorehosur.appajigowda@intel.com>
2017-03-12 20:25:57 -0700

    WIP Added class defination for 'Model' and methods to generate train and test prototext from the defined caffe net.
    note: need more inputs to make the classes more abstract and to retain the flexibility for the user to define layers

--- dlmodel.py
+++ dlmodel.py
@@ -3,9 +3,37 @@
 import sys
 import json
 import dlutil
-
+import caffe
 from pprint import pprint
 from subprocess import Popen, PIPE
+
+
+class Model(object):
+    def __init__(self, model_name, dataset_name):
+        self.name = name
+        self.dataset_name = dataset_name
+
+    def write_train_net(self, batch_size):
+        with open('self.dataset_name/lenet_auto_train.prototxt', 'w') as f:
+            f.write(str(lenet('mnist/mnist_train_lmdb', batch_size)))
+
+    def write_test_net(self, batch_size):
+        with open('mnist/lenet_auto_test.prototxt', 'w') as f:
+            f.write(str(lenet('mnist/mnist_test_lmdb', batch_size)))
+
+    """def net(self, source, batch_size, net_layers=list()):
+    # our version of LeNet: a series of linear and simple nonlinear transformations
+        from caffe import layers as L, params as P
+        n = caffe.NetSpec()
+        n.data, n.label = L.Data(batch_size=batch_size, backend=P.Data.LMDB, source=source,
+                                     transform_param=dict(scale=1./255), ntop=2)
+
+        for each_layer in net_layers:
+            
+        n.conv1 = L.Convolution(n.data, kernel_size=5, num_output=20, weight_filler=dict(type='xavier'))
+    """ 
+
+
 
 def get_model_hashtable():
         "Returns the model hashtable"
